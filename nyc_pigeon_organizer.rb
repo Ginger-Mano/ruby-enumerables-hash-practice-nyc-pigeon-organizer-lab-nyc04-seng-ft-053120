bc810756cecccd53307f754b5ce532ddfa32f26d
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-15 21:06:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = data.each_with_object({}) do |(key, value), new_hash| #creates a new hash. Attached variable to new_hash to return later
+    value.each do |inner_key, names| #inner keys are color, gender, habitat. Names are the values
+      names.each do |name| #names that should become keys, fro the values in the previous loop
+        if !new_hash[name] #says if there is no hash called name, create it as a new hash
+          new_hash[name] = {}
+        end
+        if !new_hash[name][key] #says if there is no hash of names with a hash of an array, create a new array for that hash
+          !new_hash[name][key] = []
+        end
+        new_hash[name][key] << inner_key.to_s #return the full hash with the values pushed in as a string
+      end
+    end
+  end
 end
